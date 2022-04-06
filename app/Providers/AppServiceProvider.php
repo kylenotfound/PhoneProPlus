cc9014cb21d45ee32388e31ce8e49675fa4e3b6f
Kyle Evangelisto <kyleevangelisto@gmail.com>
2022-04-05 22:37:14 -0400

    forgot to add url generator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider {
@@ -22,7 +23,7 @@ class AppServiceProvider extends ServiceProvider {
      *
      * @return void
      */
-    public function boot() {
+    public function boot(UrlGenerator $url) {
 
         if (env('REDIRECT_HTTPS')) {
             $url->formatScheme('https');
